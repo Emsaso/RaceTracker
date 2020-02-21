43d9d1deaf6248fb9f8f1cd326984d7c0e992f6a
Emsaso <34371682+Emsaso@users.noreply.github.com>
2020-02-21 10:50:55 +0100

    Update MainPage.xaml.cs

--- RaceTracker/MainPage.xaml.cs
+++ RaceTracker/MainPage.xaml.cs
@@ -20,7 +20,7 @@ namespace RaceTracker
     public partial class MainPage : ContentPage
     {
         readonly INotificationManager _notificationManager;
-        int _notificationNumber = 0;
+        int _notificationNumber;
 
         public MainPage()
         {
@@ -30,40 +30,26 @@ namespace RaceTracker
             _notificationManager.NotificationReceived += (sender, eventArgs) =>
             {
                 var evtData = (NotificationEventArgs)eventArgs;
-                //ShowNotification(evtData.Title, evtData.Message);
                 ShowNotification(evtData.Title, evtData.Message);
             };
         }
         private void PushNotificationTest(object sender, EventArgs e)
         {
             var raceTrackerUrl = "https://racetracker.no/";
-
-            //Uri uri = new Uri(raceTrackerUrl);
-            //await Browser.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
-            //Analytics.TrackEvent("My custom event");
-            //Analytics.TrackEvent("ios=2ce226c4-0805-4453-b670-1940f04bd4e7;android=android=6da72cba-ff85-4cfa-b9c5-d0b6146ea3c9", new Dictionary<string, string>(_amOfPushSent));
-            //Analytics.TrackEvent($"Push Event Test {_amOfPushSent}, {raceTrackerUrl}", new Dictionary<string, string>());
-            //Analytics.TrackEvent($"Push Event Test {_amOfPushSent}");
-
             Guid newGuid = Guid.NewGuid();
-            //Analytics.TrackEvent($"{newGuid}");
 
             _notificationNumber++;
             TestLabel.Text = "Push request test succeeded " + _notificationNumber;
             string title = $"Checkpoint #{_notificationNumber}";
-            string message = $"{raceTrackerUrl}";
-            //string message = $"{raceTrackerUrl}{newGuid}";
+            string message = $"{raceTrackerUrl}{newGuid}";
             _notificationManager.ScheduleNotification(title, message);
             Analytics.TrackEvent($"{_notificationManager}, {newGuid}");
-
-            //bool isEnabled = await Push.IsEnabledAsync();
-
-            //Crashes.GenerateTestCrash();
         }
         void ShowNotification(string title, string message)
         {
             Device.BeginInvokeOnMainThread(() =>
             {
+                Browser.OpenAsync(message, BrowserLaunchMode.SystemPreferred);
                 var ttl = new Label()
                 {
                     Text = $"{title}:",
@@ -71,9 +57,8 @@ namespace RaceTracker
                 };
                 var msg = new Label()
                 {
-                    //Text = $"Notification Received:\nTitle: {title}\nMessage: {message}"
-                    //Text = $"{message}",
-                    Text = $"Link",
+                    //Text = $"Link",
+                    Text = $"{message}",
                     TextColor = Color.Blue,
                     TextDecorations = TextDecorations.Underline,
                     GestureRecognizers = { new TapGestureRecognizer
